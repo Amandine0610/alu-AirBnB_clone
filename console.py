dbba9983e0b8a1f8976920d66320d30e050f840c
Amandine0610 <a.irakoze@alustudent.com>
2024-05-21 13:44:54 +0200

    Updated

--- console.py
+++ console.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Defines the command interpreter for the AirBnB clone project.
-"""
-
+"""Defines AirBnb console."""
 import cmd
 import re
 from shlex import split
@@ -16,10 +13,7 @@ from models.amenity import Amenity
 from models.review import Review
 
 
-def parse_arguments(arg):
-    """
-    Parse a given argument string and return a list of arguments.
-    """
+def parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
     brackets = re.search(r"\[(.*?)\]", arg)
     if curly_braces is None:
@@ -38,12 +32,14 @@ def parse_arguments(arg):
 
 
 class HBNBCommand(cmd.Cmd):
-    """
-    Defines the command interpreter for AirBnB.
+    """Defines command interpreter.
+
+    Attributes:
+        prompt (str): The command prompt.
     """
 
     prompt = "(hbnb) "
-    available_classes = {
+    __classes = {
         "BaseModel",
         "User",
         "State",
@@ -55,13 +51,13 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """
-        Override the emptyline method to do nothing.
+            Do nothing upon receiving an empty line
         """
         pass
 
     def default(self, arg):
         """
-        Default behavior for unknown commands.
+            Default behavior for cmd module when input is invalid
         """
         argdict = {
             "all": self.do_all,
@@ -76,7 +72,7 @@ class HBNBCommand(cmd.Cmd):
             match = re.search(r"\((.*?)\)", argl[1])
             if match is not None:
                 command = [argl[1][:match.span()[0]], match.group()[1:-1]]
-                if command[0] in argdict:
+                if command[0] in argdict.keys():
                     call = "{} {}".format(argl[0], command[1])
                     return argdict[command[0]](call)
         print("*** Unknown syntax: {}".format(arg))
@@ -84,26 +80,26 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """
-        Quit command to exit the program.
+            Quit command to exit the program.
         """
         return True
 
     def do_EOF(self, arg):
         """
-        EOF signal to exit the program.
+            EOF signal to exit the program.
         """
         print("")
         return True
 
     def do_create(self, arg):
         """
-        Create a new class instance and print its id.
         Usage: create <class>
+        Create a new class instance and print its id.
         """
-        argl = parse_arguments(arg)
+        argl = parse(arg)
         if len(argl) == 0:
             print("** class name missing **")
-        elif argl[0] not in AirBnBCommand.available_classes:
+        elif argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
             print(eval(argl[0])().id)
@@ -111,14 +107,14 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, arg):
         """
-        Display the string representation of a class instance of a given id.
         Usage: show <class> <id> or <class>.show(<id>)
+        Display the string representation of a class instance of a given id.
         """
-        argl = parse_arguments(arg)
+        argl = parse(arg)
         objdict = storage.all()
         if len(argl) == 0:
             print("** class name missing **")
-        elif argl[0] not in AirBnBCommand.available_classes:
+        elif argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         elif len(argl) == 1:
             print("** instance id missing **")
@@ -129,18 +125,18 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, arg):
         """
-        Delete a class instance of a given id.
         Usage: destroy <class> <id> or <class>.destroy(<id>)
+        Delete a class instance of a given id.
         """
-        argl = parse_arguments(arg)
+        argl = parse(arg)
         objdict = storage.all()
         if len(argl) == 0:
             print("** class name missing **")
-        elif argl[0] not in AirBnBCommand.available_classes:
+        elif argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         elif len(argl) == 1:
             print("** instance id missing **")
-        elif "{}.{}".format(argl[0], argl[1]) not in objdict:
+        elif "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
             print("** no instance found **")
         else:
             del objdict["{}.{}".format(argl[0], argl[1])]
@@ -148,12 +144,12 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, arg):
         """
+        Usage: all or all <class> or <class>.all()
         Display string representations of all instances of a given class.
         If no class is specified, displays all instantiated objects.
-        Usage: all or all <class> or <class>.all()
         """
-        argl = parse_arguments(arg)
-        if len(argl) > 0 and argl[0] not in AirBnBCommand.available_classes:
+        argl = parse(arg)
+        if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
             objl = []
@@ -166,34 +162,37 @@ class HBNBCommand(cmd.Cmd):
 
     def do_count(self, arg):
         """
-        Retrieve the number of instances of a given class.
         Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class.
         """
-        argl = parse_arguments(arg)
-        count = sum(1 for obj in storage.all().values() if argl[0] == obj.__class__.__name__)
+        argl = parse(arg)
+        count = 0
+        for obj in storage.all().values():
+            if argl[0] == obj.__class__.__name__:
+                count += 1
         print(count)
 
     def do_update(self, arg):
         """
+        Usage: update <class> <id> <attribute_name> <attribute_value> or
+       <class>.update(<id>, <attribute_name>, <attribute_value>) or
+       <class>.update(<id>, <dictionary>)
         Update a class instance of a given id by adding or updating
         a given attribute key/value pair or dictionary.
-        Usage: update <class> <id> <attribute_name> <attribute_value> or
-               <class>.update(<id>, <attribute_name>, <attribute_value>) or
-               <class>.update(<id>, <dictionary>)
         """
-        argl = parse_arguments(arg)
+        argl = parse(arg)
         objdict = storage.all()
 
         if len(argl) == 0:
             print("** class name missing **")
             return False
-        if argl[0] not in AirBnBCommand.available_classes:
+        if argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
             return False
         if len(argl) == 1:
             print("** instance id missing **")
             return False
-        if "{}.{}".format(argl[0], argl[1]) not in objdict:
+        if "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
             print("** no instance found **")
             return False
         if len(argl) == 2:
@@ -206,17 +205,18 @@ class HBNBCommand(cmd.Cmd):
                 print("** value missing **")
                 return False
 
-        obj = objdict["{}.{}".format(argl[0], argl[1])]
         if len(argl) == 4:
-            if argl[2] in obj.__class__.__dict__:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            if argl[2] in obj.__class__.__dict__.keys():
                 valtype = type(obj.__class__.__dict__[argl[2]])
                 obj.__dict__[argl[2]] = valtype(argl[3])
             else:
                 obj.__dict__[argl[2]] = argl[3]
-        elif isinstance(eval(argl[2]), dict):
+        elif type(eval(argl[2])) == dict:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
             for k, v in eval(argl[2]).items():
-                if (k in obj.__class__.__dict__ and
-                        isinstance(obj.__class__.__dict__[k], (str, int, float))):
+                if (k in obj.__class__.__dict__.keys() and
+                        type(obj.__class__.__dict__[k]) in {str, int, float}):
                     valtype = type(obj.__class__.__dict__[k])
                     obj.__dict__[k] = valtype(v)
                 else:
@@ -226,4 +226,3 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
-
